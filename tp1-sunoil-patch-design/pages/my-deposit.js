5f3bc1fec78a1d9b31888ae32ef32243973442c3
sunoil <98844638+sunoil@users.noreply.github.com>
2026-02-02 21:28:10 +0200

    Remove unused Image import from my-deposit.js

--- tp1-sunoil-patch-design/pages/my-deposit.js
+++ tp1-sunoil-patch-design/pages/my-deposit.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
